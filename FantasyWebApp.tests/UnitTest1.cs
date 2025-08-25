b1399343d94fb7c528d7dad01e986226bcd39fdf
tarunb-0127 <tarun.krithikabalaji@gmail.com>
2025-08-25 11:02:58 +0530

    Added a test

---
+++ FantasyWebApp.tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FantasyWebApp.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
